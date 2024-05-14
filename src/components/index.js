f95b1b8dfd49fc15656648b8888e2d9a2d8a0142
Slava Katiukha <v.katiukha@gmail.com>
2024-05-14 20:09:00 +0200

    feat: add prefix export to settings in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { prefix } from "./settings";
 export { default as Avatar } from "./Avatar";
 export { default as AvatarGroup } from "./AvatarGroup";
 export { default as ChatContainer } from "./ChatContainer";
